3f3390b5c21f3f8a1dde529d06bd39e6a4a4e28c
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2025-01-05 15:53:29 +0900

    Fix error

--- src/main/java/net/minecraft/util/Cartesian.java
+++ src/main/java/net/minecraft/util/Cartesian.java
@@ -57,12 +57,15 @@ public class Cartesian {
 			this.iterables = iterables;
 		}
 
-		public Iterator<T[]> iterator() {
-			return (Iterator<T[]>) (this.iterables.length <= 0
-					? Collections.singletonList((Object[]) Cartesian.createArray(this.clazz, 0)).iterator()
-					: new Cartesian.Product.ProductIterator(this.clazz, this.iterables));
-		}
-
+	    public Iterator<T[]> iterator() {
+	        if (this.iterables.length <= 0) {
+	            T[] emptyArray = Cartesian.createArray(this.clazz, 0);
+	            return Collections.singletonList(emptyArray).iterator();
+	        } else {
+	            return new ProductIterator<>(this.clazz, this.iterables);
+	        }
+	    }
+	    
 		static class ProductIterator<T> extends UnmodifiableIterator<T[]> {
 			private int index;
 			private final Iterable<? extends T>[] iterables;
