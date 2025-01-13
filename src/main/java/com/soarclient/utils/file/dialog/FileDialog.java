3815a636cb219b80ddc05c3323c4db44462ba7c3
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2025-01-13 12:09:36 +0900

    Update FileDialog.java

--- src/main/java/com/soarclient/utils/file/dialog/FileDialog.java
+++ src/main/java/com/soarclient/utils/file/dialog/FileDialog.java
@@ -13,55 +13,46 @@ public class FileDialog {
 
 	private DialogStatus status;
 	private File output;
-	private String errorMessage;
 
 	public FileDialog() {
 		status = DialogStatus.ERROR;
 	}
 
-	public void openFileDialog(String dialogTitle, String defaultPath, String... extensions) {
-		
-		try (MemoryStack stack = MemoryStack.stackPush()) {
-			NFDFilterItem.Buffer filters = NFDFilterItem.malloc(1, stack);
-			filters.get(0).name(stack.UTF8(dialogTitle)).spec(stack.UTF8(String.join(",", extensions)));
+	public void openFile(String name, String... extensions) {
 
-			ByteBuffer defaultPathBuffer = defaultPath != null ? MemoryUtil.memUTF8(defaultPath) : null;
+		try (MemoryStack stack = MemoryStack.stackPush()) {
+
+			NFDFilterItem.Buffer filters = NFDFilterItem.malloc(1);
+
+			filters.get(0).name(stack.UTF8(name)).spec(stack.UTF8(String.join(",", extensions)));
 
 			PointerBuffer pp = stack.mallocPointer(1);
-			parseResult(NativeFileDialog.NFD_OpenDialog(pp, filters, defaultPathBuffer), pp);
-		} catch (Exception e) {
-			status = DialogStatus.ERROR;
-			errorMessage = e.getMessage();
+
+			parseResult(NativeFileDialog.NFD_OpenDialog(pp, filters, (ByteBuffer) null), pp);
 		}
 	}
 
-	public void openFolderDialog(String defaultPath) {
-		
-		PointerBuffer path = MemoryUtil.memAllocPointer(1);
-		
-		try {
-			ByteBuffer defaultPathBuffer = defaultPath != null ? MemoryUtil.memUTF8(defaultPath) : null;
+	public void openFolder() {
 
-			parseResult(NativeFileDialog.NFD_PickFolder(path, defaultPathBuffer), path);
-		} catch (Exception e) {
-			status = DialogStatus.ERROR;
-			errorMessage = e.getMessage();
+		PointerBuffer path = MemoryUtil.memAllocPointer(1);
+
+		try {
+			parseResult(NativeFileDialog.NFD_PickFolder(path, (ByteBuffer) null), path);
 		} finally {
 			MemoryUtil.memFree(path);
 		}
 	}
 
 	private void parseResult(int result, PointerBuffer path) {
-		
+
 		switch (result) {
 		case NativeFileDialog.NFD_OKAY:
+
 			File file = new File(path.getStringUTF8(0));
+
 			if (file != null && file.exists()) {
 				status = DialogStatus.SUCCESS;
 				output = file;
-			} else {
-				status = DialogStatus.ERROR;
-				errorMessage = "Selected file does not exist.";
 			}
 			break;
 		case NativeFileDialog.NFD_CANCEL:
@@ -69,7 +60,6 @@ public class FileDialog {
 			break;
 		case NativeFileDialog.NFD_ERROR:
 			status = DialogStatus.ERROR;
-			errorMessage = NativeFileDialog.NFD_GetError();
 			break;
 		}
 	}
@@ -88,9 +78,5 @@ public class FileDialog {
 
 	public File getOutput() {
 		return output;
-	}
-
-	public String getErrorMessage() {
-		return errorMessage;
 	}
 }
