c8b05708f6e87b5be2dd8fe28dbf805de1f193fa
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2025-01-20 21:30:08 +0900

    Add taplook

--- src/main/java/com/soarclient/management/mod/ModManager.java
+++ src/main/java/com/soarclient/management/mod/ModManager.java
@@ -40,6 +40,7 @@ import com.soarclient.management.mod.impl.player.LeftHandMod;
 import com.soarclient.management.mod.impl.player.MouseDelayFixMod;
 import com.soarclient.management.mod.impl.player.NoJumpDelayMod;
 import com.soarclient.management.mod.impl.player.SnapTapMod;
+import com.soarclient.management.mod.impl.player.TaplookMod;
 import com.soarclient.management.mod.impl.player.ToggleSneakMod;
 import com.soarclient.management.mod.impl.player.ToggleSprintMod;
 import com.soarclient.management.mod.impl.player.ZoomMod;
@@ -98,6 +99,7 @@ public class ModManager {
 		mods.add(new MouseDelayFixMod());
 		mods.add(new NoJumpDelayMod());
 		mods.add(new SnapTapMod());
+		mods.add(new TaplookMod());
 		mods.add(new ToggleSneakMod());
 		mods.add(new ToggleSprintMod());
 		mods.add(new ZoomMod());
