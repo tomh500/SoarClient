9d1b1562db98b0f0028ba52e13e8fa4f71f0811a
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2025-02-01 22:40:08 +0900

    Revert 1.8.9 optifine

--- src/main/java/com/soarclient/ui/component/handler/impl/HctColorPickerHandler.java
+++
@@ -1,8 +0,0 @@
-package com.soarclient.ui.component.handler.impl;
-
-import com.soarclient.libraries.material3.hct.Hct;
-import com.soarclient.ui.component.handler.ComponentHandler;
-
-public abstract class HctColorPickerHandler extends ComponentHandler {
-	public abstract void onPicking(Hct hct);
-}
--- src/main/java/com/soarclient/ui/component/handler/impl/KeybindHandler.java
+++
@@ -1,9 +0,0 @@
-package com.soarclient.ui.component.handler.impl;
-
-import com.soarclient.ui.component.handler.ComponentHandler;
-
-import net.minecraft.client.util.InputUtil.Key;
-
-public abstract class KeybindHandler extends ComponentHandler {
-	public abstract void onBinded(Key key);
-}
