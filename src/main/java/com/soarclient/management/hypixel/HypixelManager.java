0a1053452b43be3c2048e344a11db2cce7ff9e99
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2025-02-05 18:04:53 +0900

    I forgot register event

--- src/main/java/com/soarclient/management/hypixel/HypixelManager.java
+++ src/main/java/com/soarclient/management/hypixel/HypixelManager.java
@@ -2,11 +2,17 @@ package com.soarclient.management.hypixel;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.soarclient.event.EventBus;
 import com.soarclient.management.hypixel.api.HypixelUser;
+import com.soarclient.management.hypixel.handler.HypixelHandler;
 
 public class HypixelManager {
 
 	private final Cache<String, HypixelUser> cache = Caffeine.newBuilder().maximumSize(200).build();
+	
+	public HypixelManager() {
+		EventBus.getInstance().register(new HypixelHandler());
+	}
 	
 	public void add(HypixelUser user) {
 		cache.put(user.getUuid(), user);
