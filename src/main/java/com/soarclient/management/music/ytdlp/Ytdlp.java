e06d7fbc93904743bd393e343726b779188fcce9
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2025-02-13 22:24:24 +0900

    Add ytdlp base

---
+++ src/main/java/com/soarclient/management/music/ytdlp/Ytdlp.java
@@ -0,0 +1,63 @@
+package com.soarclient.management.music.ytdlp;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.soarclient.utils.file.FileLocation;
+
+public class Ytdlp {
+
+	private String ytdlp;
+	private String ffmpeg;
+
+	public void setYtdlpPath(File file) {
+		this.ytdlp = file.getAbsolutePath();
+	}
+
+	public void setYtdlpCommand(String command) {
+		this.ytdlp = command;
+	}
+
+	public void setFFmpegPath(File file) {
+		this.ffmpeg = file.getAbsolutePath();
+	}
+
+	public boolean download(String url) {
+
+		List<String> command = new ArrayList<>();
+
+		command.add(ytdlp);
+        command.add("-f");
+        command.add("bestaudio");
+		command.add("--extract-audio");
+		command.add("--audio-format");
+		command.add("flac");
+		command.add("--embed-thumbnail");
+		command.add("--add-metadata");
+
+		if (ffmpeg != null && !ffmpeg.isBlank() && !ffmpeg.isEmpty()) {
+			command.add("--ffmpeg-location");
+			command.add(ffmpeg);
+		}
+
+		command.add("--output");
+		command.add(FileLocation.MUSIC_DIR + File.separator + "%(title)s.%(ext)s");
+
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder(command);
+			Process process = processBuilder.start();
+
+			int exitCode = process.waitFor();
+
+			if (exitCode == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
